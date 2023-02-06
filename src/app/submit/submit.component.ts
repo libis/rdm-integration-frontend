6c762fd6697b597c1e31a538806bcfc261b51800
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2023-02-06 10:10:29 +0100

    pulling slower for submit status

--- src/app/submit/submit.component.ts
+++ src/app/submit/submit.component.ts
@@ -54,7 +54,7 @@ export class SubmitComponent implements OnInit {
   }
 
   getDataSubscripion(): Subscription {
-    return interval(1000)
+    return interval(5000)
       .pipe(
         switchMap(() => this.dataUpdatesService.updateData(this.data, this.pid))
       ).subscribe({
