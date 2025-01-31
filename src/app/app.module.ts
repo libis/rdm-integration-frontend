cd4a61ef6488f7c2b46311825b9257f522f27f05
Eryk Kullikowski <eryk.kulikowski@gmail.com>
2025-01-31 11:30:45 +0100

    color schema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Lara from '@primeng/themes/lara';
+import { definePreset } from '@primeng/themes';
 
 @NgModule({
   declarations: [
@@ -75,9 +76,59 @@ import Lara from '@primeng/themes/lara';
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-          preset: Lara
+        preset: definePreset(Lara, {
+          semantic: {
+            primary: {
+              50: '{blue.50}',
+              100: '{blue.100}',
+              200: '{blue.200}',
+              300: '{blue.300}',
+              400: '{blue.400}',
+              500: '{blue.500}',
+              600: '{blue.600}',
+              700: '{blue.700}',
+              800: '{blue.800}',
+              900: '{blue.900}',
+              950: '{blue.950}'
+            },
+            colorScheme: {
+              light: {
+                surface: {
+                  0: '#ffffff',
+                  50: '{zinc.50}',
+                  100: '{zinc.100}',
+                  200: '{zinc.200}',
+                  300: '{zinc.300}',
+                  400: '{zinc.400}',
+                  500: '{zinc.500}',
+                  600: '{zinc.600}',
+                  700: '{zinc.700}',
+                  800: '{zinc.800}',
+                  900: '{zinc.900}',
+                  950: '{zinc.950}'
+                }
+              },
+              dark: {
+                surface: {
+                  0: '#ffffff',
+                  50: '{zinc.50}',
+                  100: '{zinc.100}',
+                  200: '{zinc.200}',
+                  300: '{zinc.300}',
+                  400: '{zinc.400}',
+                  500: '{zinc.500}',
+                  600: '{zinc.600}',
+                  700: '{zinc.700}',
+                  800: '{zinc.800}',
+                  900: '{zinc.900}',
+                  950: '{zinc.950}'
+                }
+              }
+            }
+          }
+        }),
       }
-  })
+    })
   ],
   bootstrap: [AppComponent]
 })
